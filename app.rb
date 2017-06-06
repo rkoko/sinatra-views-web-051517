4ceba5b7b0356c9375148ab1cdb744ce9dabfa17
rkoko <rinda.ko@gmail.com>
2017-06-05 23:10:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+		#render a file called index.erb inside a directory called views
+	end
+
+	get "/info" do
+		erb :info
 	end
 end
